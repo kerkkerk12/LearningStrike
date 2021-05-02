fd79a4f0000e4f7eac215ce5aefa8d6fe5f86292
imron yusoh <lucy_oliv@outlook.com>
2021-05-02 23:47:41 +0700

    แก้ Alert ของแมน แต่ยังไม่ได้แก้ตอนรีเฟรช

--- web/src/components/Home.js
+++ web/src/components/Home.js
@@ -1,31 +1,30 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar';
 import Rooms from './Rooms';
+import auth from "./AUTH/auth";
 // import TextField from "@material-ui/core/TextField"
 import { useEffect, useState } from "react"
 import {useLocation} from "react-router-dom"
 import { Route, Router } from 'react-router';
 
 export default function Home(props) {
-
-    const location = useLocation()
-    const isLogin = props.location.state.isLogin
-    console.log("isLogin =",isLogin)
-    const [showAlert,setShowAlert] = useState(false)
     
+    const [showAlert,setShowAlert] = useState(false);
     const alertLogin = () => {
         return alert("login success")
     }
+    
     useEffect(() => {
-        if (isLogin) setShowAlert(true)
-        
+        if (auth.isAuthenticated()) {
+            setShowAlert(true)
+        }
     }, [])
     return (
         
         
         <div style={{ backgroundImage: 'url(/images/YellowWallpaper3.png)', height: "150vh", backgroundSize: "cover" }}>
             <Navbar></Navbar>
-            {showAlert && <div>{alertLogin()}</div>}    
+            {showAlert && auth.loginAlert() &&<div>{alertLogin()}</div>}    
             <div style={{ marginLeft: "1.7%", color: "#000000", fontFamily:"Lucida Bright",paddingTop: "3%" ,width: "130px",padding: "10px",border: "5px solid black", margin: "12px",marginTop: "8px"}}>
                 <h2>HOME</h2>
                
