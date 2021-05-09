babeef456083f695bfc3b334cb963da2f982b07c
Thanadol supanno <60128303+thanadoltu@users.noreply.github.com>
2021-05-10 01:55:51 +0700

    add home work and home work bar

--- web/src/components/Todo.js
+++ web/src/components/Todo.js
@@ -16,9 +16,15 @@ export default class Todo extends Component {
     render() {
         return (<div style={{ backgroundImage: 'url(/images/background.jpg)', width: "100vw", height: "100vh", backgroundSize: "cover" }}>
             <Navbar></Navbar>
+            <div style={{ marginLeft: "1.7%", color: "#000000", fontFamily:"Adobe Fan Heiti Std ",paddingTop: "3%" ,width: "130px",padding: "10px",border: "5px solid black", margin: "12px",marginTop: "20px",}}>
+               
+                <h2>HOME WORK</h2>
+            </div>
+            
+            
 
             <div>{this.state.homework.map(Todo => {
-                return (<div style={{ marginLeft: "30%", marginRight: "30%", marginTop: "2%", textAlign: 'center' }}>
+                return (<div style={{ marginLeft: "30%", marginRight: "30%", marginTop: "2%", textAlign: 'center',maxWidth: '45%' ,border: "5px solid black",marginLeft:"27%" }}>
                 <ul className="list-group">
 
                     <li className="list-group-item">HomeWork {Todo}</li>
