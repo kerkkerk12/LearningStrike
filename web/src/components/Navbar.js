564958ff1ab829dfc4c89805b16b39977d5d3dd5
Famp <fam.b.boy2@gmail.com>
2021-05-11 00:14:08 +0700

    Revert "kuay"
    This reverts commit 1d9e2cc241582fde5c256c1d7c200c675cfca9a4.

--- web/src/components/Navbar.js
+++ web/src/components/Navbar.js
@@ -25,7 +25,6 @@ export default function MenuAppBar() {
     auth.logout();
     localStorage.clear()
     window.location.reload();
-<<<<<<< HEAD
   };
 
   const handleClose = () => {
@@ -78,44 +77,6 @@ export default function MenuAppBar() {
                 <MenuItem>{window.email}</MenuItem>
                 <MenuItem onClick={onClick}>Logout</MenuItem>
               </Menu>
-=======
-  }
-  render() {
-    return (
-      <div>
-        <div style={{backgroundColor: "#000000" ,border: "1px solid"}}></div>
-        <div style={{backgroundColor: "#000000" ,border: "1px solid"}}></div>
-        <nav
-          className="navbar navbar-expand-lg navbar-light"
-          style={{ backgroundColor: "#ffb137",fontFamily:"Adobe Fan Heiti Std " }}
-        >
-          <div className="container-fluid">
-            <span className="navbar-brand" ><Link to = "/home" style={{ color: 'inherit', textDecoration: 'inherit'}}><h1>Learning Strike</h1></Link></span>
-            <div className="d-grid gap-2 d-md-flex justify-content-md-end">
-              <a style={{ marginRight: "30px", marginTop: "auto" }}><Link to = "/home" style={{ color: 'kram blue', textDecoration: 'inherit'}}><h2>Home</h2></Link></a>
-              <a style={{ marginRight: "30px", marginTop: "auto" }}><Link to = "/editroom" style={{ color: 'kram blue', textDecoration: 'inherit'}}><h2>Edit Room</h2></Link></a>
-              <a style={{ marginRight: "30px", marginTop: "auto" }}><Link to = "/todo" style={{ color: 'kram blue', textDecoration: 'inherit'}}><h2>To do work</h2></Link></a>
-              
-              <h2 style={{marginTop: "auto",fontFamily:"Adobe Fan Heiti Std",color:"#000000"}}><b>{window.email}&nbsp;</b></h2>
-             
-              <img
-                src={process.env.PUBLIC_URL + "/images/usericon.png"}
-                style={{width: "60px" }}
-                
-              ></img>
-              &nbsp;&nbsp;
-              <a
-                className="btn btn-danger me-md-2"
-                type="button"
-                onClick={this.onClick}
-                data-bs-toggle="modal"
-                style={{ margin: "auto" ,color:"white"}}
-              >
-
-                Logout
-                
-              </a>
->>>>>>> parent of 915e1c0e (change navbar logo)
             </div>
           </div>
         </Toolbar>
