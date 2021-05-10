cc83164046a8cd59606c1fdafa026890af074c07
Famp <fam.b.boy2@gmail.com>
2021-05-11 00:14:06 +0700

    Revert "kuay"
    This reverts commit ef91771ff0b717ba040319e110d7c377d2bd4377.

--- web/src/components/Navbar.js
+++ web/src/components/Navbar.js
@@ -1,20 +1,30 @@
-import { Component } from "react";
+import React from "react";
+
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+
+
 import auth from "./AUTH/auth";
 import { Link } from 'react-router-dom';
-import React from 'react'
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
 
-export default class Navbar extends Component {
-  constructor(props) {
-    super(props);
-    this.onClick = this.onClick.bind(this);
-  }
+export default function MenuAppBar() {
 
-  onClick() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const onClick = () => {
     auth.logout();
     localStorage.clear()
     window.location.reload();
-<<<<<<< HEAD
 <<<<<<< HEAD
   };
 
@@ -106,48 +116,10 @@ export default class Navbar extends Component {
                 
               </a>
 >>>>>>> parent of 915e1c0e (change navbar logo)
-=======
-  }
-  render() {
-    return (
-      <div>
-        <div style={{backgroundColor: "#000000" ,border: "1px solid"}}></div>
-        <div style={{backgroundColor: "#000000" ,border: "1px solid"}}></div>
-        <nav
-          className="navbar navbar-expand-lg navbar-light"
-          style={{ backgroundColor: "#ffb137",fontFamily:"Adobe Fan Heiti Std " }}
-        >
-          <div className="container-fluid">
-            <span className="navbar-brand" ><Link to = "/home" style={{ color: 'inherit', textDecoration: 'inherit'}}><b><h1>Learning Strike</h1></b></Link></span>
-            <div className="d-grid gap-2 d-md-flex justify-content-md-end">
-              <a style={{ marginRight: "30px", marginTop: "auto" }}><Link to = "/home" style={{ color: 'kram blue', textDecoration: 'inherit'}}><h2>Home</h2></Link></a>
-              <a style={{ marginRight: "30px", marginTop: "auto" }}><Link to = "/editroom" style={{ color: 'kram blue', textDecoration: 'inherit'}}><h2>Edit Room</h2></Link></a>
-              <a style={{ marginRight: "30px", marginTop: "auto" }}><Link to = "/todo" style={{ color: 'kram blue', textDecoration: 'inherit'}}><h2>To do work</h2></Link></a>
-              
-              <h2 style={{marginTop: "auto",fontFamily:"Adobe Fan Heiti Std",color:"#000000"}}><b>{window.email}&nbsp;</b></h2>
-             
-              <img
-                src={process.env.PUBLIC_URL + "/images/usericon.png"}
-                style={{width: "60px" }}
-                
-              ></img>
-              &nbsp;&nbsp;
-              <a
-                className="btn btn-danger me-md-2"
-                type="button"
-                onClick={this.onClick}
-                data-bs-toggle="modal"
-                style={{ margin: "auto" ,color:"white"}}
-              >
-                Logout
-              </a>
->>>>>>> parent of c750f45e (ปรับnavbarนิดหน่อย/editroom)
             </div>
           </div>
-        </nav>
-        <div style={{backgroundColor: "#000000" ,border: "1px solid"}}></div>
-        <div style={{backgroundColor: "#000000" ,border: "1px solid"}}></div>
-      </div>
-    );
-  }
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
